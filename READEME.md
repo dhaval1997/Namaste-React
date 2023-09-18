b83896d0c2b546477015c429130c10e83cc747a8
dhaval1997 <drprajapati000@gmail.com>
2023-09-18 09:42:58 +0530

    React Components

---
+++ READEME.md
@@ -0,0 +1,20 @@
+# Namaste React
+
+
+# Parcel    
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File watching Algorithm - written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Bundling
+- Compress
+- Minification
+- Code Splitting
+- Differential Bundling - Suppore Older Browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove Unused code
+- Different dev and prod bundles
--- app.js
+++ app.js
@@ -1,23 +1,33 @@
-//create an html element using JavaScript.
+import React from "react";
+import ReactDOM from "react-dom";
 
-// const heading = document.createElement("h1");
-// heading.innerHTML = "Namaste React Using JavaScript";
+// jsx transpiled before it reaches the js - Parcel - Babel
 
-// const rootDir = document.getElementById("root");
-// rootDir.appendChild(heading);
+// jsx => Babel transpiles it to React.createElement => ReactElement-js Object => HTML element(render)
+// Everything is React is Component
 
-// create an element using React
-// syntax: React.createElement("HTML tag", {set attributes}, "context and child element")
-const heading = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", {id:"child"}, [
-      React.createElement("h2", {}, "this is h2 tag"),
-      React.createElement("h1", {}, "this is h1  tag"),
-    ]),
-  React.createElement("div", {}, [
-    React.createElement("h2", {}, "this is h2 tag"),
-    React.createElement("h1", {}, "this is h1  tag"),
-  ]),
- ]); //'heading' is react element which is JS object.
-const root = ReactDOM.createRoot(document.getElementById("root")); //ReactDOM is from reactDom library
-console.log(heading); //  JS objects
-root.render(heading); //render means it converts React element(object) into HTML(browser understand)
+// React Functional component
+
+const HeadingComponent = () => {
+  return <h1>Namaste Functional Component</h1>;
+};
+
+const Title = () => <h1 className="head">Namaste React using jsx</h1>; //this is react functional component;
+const title = <h2 className="head">Namaste React using JavaScript</h2>; // this is js variable
+
+const number = 10000;
+const HeadingComponent2 = () => (
+  <div>
+    <h2>{console.log("Namaste")}</h2>
+    {Title()}
+    <Title></Title>
+    <Title /> // this is react functional component
+    {title} // you can inject js variable
+    <h1 id="heading">Namaste Functional Component</h1>
+  </div>
+);
+
+const heading = <h1> this is h1 using jsx </h1>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent2 />);
+  
